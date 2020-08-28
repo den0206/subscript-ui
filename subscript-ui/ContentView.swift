cb42beed071663eb465c490741868b8b6338bad0
den0206 <mac.pop@mineo.jp>
2020-08-28 20:18:41 +0900

    Initial Commit

---
+++ subscript-ui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  subscript-ui
+//
+//  Created by 酒井ゆうき on 2020/08/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ subscript-ui/subscript_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  subscript_uiApp.swift
+//  subscript-ui
+//
+//  Created by 酒井ゆうき on 2020/08/28.
+//
+
+import SwiftUI
+
+@main
+struct subscript_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
